a5411f566392ab40d5fb1a46dc1fd97db475e499
Seungjin <infinissible@gmail.com>
2022-03-02 14:46:06 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,13 +24,16 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip the fight. Goodbye!");
       // substract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(playerMoney - 10);
       console.log("playerMoney", playerMoney);
       break;
     }
   } 
     // remove enemy's health by substracting the amount set in the playerAttack variable 
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -49,7 +52,9 @@ var fight = function(enemyName) {
     }
 
     // remove player's health by substracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining"
     );
@@ -63,6 +68,13 @@ var fight = function(enemyName) {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
   }
+};
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1 ) + min);
+
+  return value;
 };
 
 // function to start a new game
@@ -81,7 +93,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
@@ -109,6 +121,8 @@ var startGame = function () {
   // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
   endGame();
 };
+
+
 
 // function to end the entire game
 var endGame = function () {
